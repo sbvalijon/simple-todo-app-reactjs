86a2f114015006e2d6b120bc224662f1fe54589f
valijon <vsobirov@exadel.com>
2020-09-18 09:01:56 +0100

    Updates

---
+++ precache-manifest.153e1485cae9f75577c21798de872eba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e567ac6049b912d59fca64d6ad154f5e",
+    "url": "/simple-todo-app-reactjs/index.html"
+  },
+  {
+    "revision": "7d05a1421ecf8d20bab7",
+    "url": "/simple-todo-app-reactjs/static/css/main.7225289e.chunk.css"
+  },
+  {
+    "revision": "08d666c9f6329e826101",
+    "url": "/simple-todo-app-reactjs/static/js/2.40cd4507.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-todo-app-reactjs/static/js/2.40cd4507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d05a1421ecf8d20bab7",
+    "url": "/simple-todo-app-reactjs/static/js/main.f4b518ea.chunk.js"
+  },
+  {
+    "revision": "c02355795ec9e2279507",
+    "url": "/simple-todo-app-reactjs/static/js/runtime-main.7043978a.js"
+  }
+]);
