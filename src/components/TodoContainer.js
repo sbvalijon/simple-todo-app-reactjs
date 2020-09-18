073e7550cf36fa9aebf73e26453a8cb063f4d638
valijon <vsobirov@exadel.com>
2020-09-18 15:04:14 +0100

    Uses data from backend instead of hardcoded todos; REST api calls for CRUD

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,26 +3,11 @@ import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false,
-      },
-    ],
+    todos: [],
   };
 
   handleChange = (id) => {
@@ -37,26 +22,41 @@ class TodoContainer extends React.Component {
   };
 
   deleteTodo = (id) => {
-    this.setState({
-      todos: [
-        ...this.state.todos.filter((todo) => {
-          return todo.id !== id;
-        }),
-      ],
-    });
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then((reponse) =>
+        this.setState({
+          todos: [
+            ...this.state.todos.filter((todo) => {
+              return todo.id !== id;
+            }),
+          ],
+        })
+      );
   };
 
   addTodoItem = (title) => {
-    const newTodo = {
-      id: uuidv4(),
-      title: title,
-      completed: false,
-    };
-
-    this.setState({
-      todos: [...this.state.todos, newTodo],
-    });
+    axios
+      .post("https://jsonplaceholder.typicode.com/todos", {
+        title: title,
+        completed: false,
+      })
+      .then((response) => {
+        this.setState({
+          todos: [...this.state.todos, response.data],
+        });
+      });
   };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos", {
+        params: {
+          _limit: 10,
+        },
+      })
+      .then((response) => this.setState({ todos: response.data }));
+  }
 
   render() {
     return (
